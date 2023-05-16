d0bf630b77b9ac726ca91df90b60d54b9ff2be25
IsaacPonce13 <111556869+IsaacPonce13@users.noreply.github.com>
2023-05-16 08:35:47 -0600

    Update server.js

--- app/server.js
+++ app/server.js
@@ -11,18 +11,36 @@ app.use(express.urlencoded({extended:true}));
 //     res.send(productos)
 // })
 
-const Producto = require('./DB/Inventario/Inventario');
-const Inventario = require('./DB/Productos/Producto');
-
-const inventario = new Inventario();
+const inventario = require('../DB/Inventario');
+const Producto = require('../DB/Productos');
 
 const producto1 = new Producto(1, 'Producto 1', 10, 100);
 const producto2 = new Producto(2, 'Producto 2', 5, 50);
 
 inventario.agregar(producto1);
 inventario.agregar(producto2);
-
-console.log(inventario.buscar(1));
+console.log(inventario);
+/*console.log(inventario.buscar(1));
 console.log(inventario.listar());
+*/
+
+app.get('/productos',(req,res)=>{
+    res.json(inventario.listar())
+})
+
+app.get('/productos/:id',(req,res)=>{
+    console.log(req.params.id)
+    res.json(inventario.buscar(req.params.id))
+})
+app.post('/productos',(req,res)=>{
+    console.log(req.body);
+    let idP=req.body.idP;
+    let nombreP=req.body.nombreP;
+    let CantP=req.body.cantP;
+    let costoP=req.body.costoP;
+    let producto=new Producto(idP,nombreP,CantP,costoP);
+    inventario.agregar(producto);
+    res.json({msg:"insercion correcta"})
+})
 
 app.listen(3000,()=>{console.log("escuchando en 3000")})
